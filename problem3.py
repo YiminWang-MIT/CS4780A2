79df2619dd7063e65ec8ba4e3c0d4a80deab01ac
Yimin Wang <yimin@rrdhcp-10-32-228-220.redrover.cornell.edu>
2013-09-23 14:43:58 -0400

    partD partE(slow ~10min) done

--- problem3.py
+++ problem3.py
@@ -119,24 +119,31 @@ def predict(tree,point):
       index+=tree[index][2]
     else: index+=tree[index][3]
   
-def rdmgen(s):
+def partition(s,k):
   snew=list(s)
-  stmp=[]
-  num=len(s)/5
-  for i in range(num):
-    index=int(random.uniform(0,len(snew)))
-    stmp.append(snew[index])
-    del snew[index]
-  return stmp
+  num=len(s)/k
+  result=[]
+  ssl=[]
+  for i in range(k):
+    stmp=[]
+    for j in range(num):
+      index=int(floor(random.uniform(0,len(snew))))
+      stmp.append(snew[index])
+      del snew[index]
+    result.append([snew+ssl,stmp])
+    ssl=ssl+stmp
+  return result
+
 #---partA---
 print 'partA'
 depthe=10000
 tree1=TDIDT(train,0,1)
-wrong=0
+wrong=0.0
 for element in test:
   ctgr=predict(tree1,element)
   if ctgr!=element[0]: wrong+=1
-print wrong
+print 'Accuracy=',1.0-wrong/len(test)
+
 #---partB---
 print 'partB'
 print 'top 2 levels'
@@ -152,9 +159,47 @@ print '    '+wordlist[leftroot[1][0]][1]
 print 'partC'
 depthe=10
 tree2=TDIDT(train,0,1)
-wrong=0
+wrong=0.0
 for element in test:
   ctgr=predict(tree2,element)
   if ctgr!=element[0]: wrong+=1
-print wrong
+print 'Accuracy=',1.0-wrong/len(test)
+
+#---partD---
+print 'partD'
+miss=[[0.0,0.0],[0.0,0.0]]
+for element in test:
+  c1=int(predict(tree1,element)==element[0])
+  c2=int(predict(tree2,element)==element[0])
+  miss[c1][c2]+=1
+print miss
+chisq=(miss[1][0]-miss[0][1])**2/(miss[0][1]+miss[1][0])
+print chisq
+
+#---partE---
+print 'partE'
+k=5
+sets=partition(train+test,k)
+sigma=[]
+for aset in sets:
+  tmptrain=aset[0]
+  tmptest=aset[1]
+  treeC=TDIDT(tmptrain,0,1)
+  treeE=TDIDT(tmptrain,0,2)
+  wrongC=0.0
+  wrongE=0.0
+  for element in tmptest:
+    ctgr=predict(treeC,element)
+    if ctgr!=element[0]: wrongC+=1
+    ctgr=predict(treeE,element)
+    if ctgr!=element[0]: wrongE+=1
+  sigma.append(((wrongC-wrongE)/len(tmptest)))
+sigmamean=0.0
+for sig in sigma: sigmamean+=sig/len(sigma)
+sigmastd=0.0
+for sig in sigma: sigmastd+=(sig-sigmamean)**2
+sigmastd=(sigmastd/(k**2-k))**0.5
+print sigma
+print 'mean=', sigmamean
+print 'std=', sigmastd
 
